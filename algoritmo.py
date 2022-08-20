16e4534dc0053d1d151378600171113a4fe8e171
Jorge Navas <jorgenavask@gmail.com>
2022-08-19 20:55:42 -0500

    Codigo finalizado y comentariado para alojar en el repositorio

--- algoritmo.py
+++ algoritmo.py
@@ -4,15 +4,11 @@ dictionary = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
              "á", "é", "í", "ó", "ú"]
 
 
-
-
 file = open(nameFile, "r")
-
 textSplit = file.read().split("   \n    ")## se separa por algo que no exista para dejar una lista de tamaño 1
-print(textSplit)
 
 
-##FILTRADO DE DATOS
+#Filtrado de datos (eliminar caracteres fuera del diccionario)
 for word in range(len(textSplit)):
     textSplit[word] = textSplit[word].lower()
     for char in range(len(textSplit[word])):
@@ -22,24 +18,18 @@ for word in range(len(textSplit)):
             textSplit[word] = textSplit[word].replace(textSplit[word][char]," ")
 
 
-print(textSplit)
-
-# x = dictionary[0].ljust(15, "x")
-# print(x)
-
+#Separacion del vector por espacios ya que no existen caracteres desconocidos
 textSplit = textSplit[0].split(" ")
 
-##PARA ELIMINAR POSICIONES VACIAS
+
+#Eliminacion de posiciones vacias en el vector
 count = 0
 for word in range(len(textSplit)):
   if len(textSplit[word]) == 0:
     count += 1
-
 for x in range(count):
   textSplit.remove("")
 
-print(textSplit)
-print(len(textSplit))
 
 #Para rellenar y completar el grupo
 for word in range(len(textSplit)):
@@ -51,9 +41,9 @@ for word in range(len(textSplit)):
     add = len(textSplit[word]) + add
     textSplit[word] = textSplit[word].ljust(add, ".")
 
-print(textSplit)
+
+#Para buscar el numero de letra en el diccionario y adjuntarlo al vector salida
 out = []
-#Para buscar el numero de letra en el diccionario
 for word in range(len(textSplit)):
   for char in range(len(textSplit[word])):
     if textSplit[word][char] in dictionary:
@@ -61,10 +51,8 @@ for word in range(len(textSplit)):
     else:
       out.append(" ")
 
-print(out)
 
-#imprimir grupos de 3
-
+#imprimir en los grupos configurados
 pos = 0
 for num in range(int(len(out)/group)):
   pos = num * group
